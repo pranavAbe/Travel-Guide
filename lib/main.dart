865bcfb363f952a99b417a1ad0429cd094b2d6b1
Pranav Abraham <mailforpranav247@gmail.com>
2021-03-21 12:30:50 +0300

    Added todo's

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+
+import 'models/app_theme.dart';
 import 'screens/app.dart';
 
 void main() => runApp(MaterialApp(
     title: 'Location Detail Page',
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(),
+    //TODO: Add an AMOLED Dark theme
+    // darkTheme: AppTheme.darkTheme,
     darkTheme: ThemeData.dark(),
     home: App()));
